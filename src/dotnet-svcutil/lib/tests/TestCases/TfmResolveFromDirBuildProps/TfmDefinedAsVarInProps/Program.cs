66e34b5935a9eda6ba1114b924931b2264cd809a
Carol Wang <v-carwan@microsoft.com>
2023-08-25 06:08:53 +0000

    Add test

---
+++ src/dotnet-svcutil/lib/tests/TestCases/TfmResolveFromDirBuildProps/TfmDefinedAsVarInProps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TfmDefinedAsVarInProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/dotnet-svcutil/lib/tests/TestCases/TfmResolveFromDirBuildProps/TfmDefinedInProps/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TfmDefinedInProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
