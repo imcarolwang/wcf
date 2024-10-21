9c7c50363ae96f941323f93fc648299adb7b9755
Carol Wang <v-carwan@microsoft.com>
2024-10-21 13:39:10 +0800

    update.

--- src/dotnet-svcutil/lib/src/Shared/Options/UpdateOptions.cs
+++ src/dotnet-svcutil/lib/src/Shared/Options/UpdateOptions.cs
@@ -134,7 +134,9 @@ namespace Microsoft.Tools.ServiceModel.Svcutil
                 var reference = this.References[idx];
                 if (reference.DependencyType == ProjectDependencyType.Project || reference.DependencyType == ProjectDependencyType.Binary)
                 {
-                    this.References[idx].FullPath = Path.Combine(optionsFileDirectory.FullName, reference.ReferenceIdentity);
+                    string fullPath = Path.GetFullPath(Path.Combine(optionsFileDirectory.FullName, reference.ReferenceIdentity));
+                    this.References[idx].FullPath = fullPath;
+                    this.References[idx].ReferenceIdentity = fullPath;
                 }
             }
         }
